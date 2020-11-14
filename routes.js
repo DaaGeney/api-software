817c89dec3282acef9b69cb41aa5500d2bc72a9b
DanielBerrioB <daniberrio10@hotmail.com>
2020-11-14 10:48:12 -0500

    Save changes

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@
 const endpoints_diego = require("./routes/endpoints_diego")
 const endpoints_juan = require("./routes/endpoints_juan")
 const riesgos = require("./routes/riesgos");
+
 module.exports = app => {
   app.use(endpoints_diego);
   app.use(endpoints_juan);
